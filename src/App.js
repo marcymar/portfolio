43907e7f55a81dc2e1d903d90c7186312ec59d18
Marcella Luz Ash <marcella@ash-photos.com>
2020-09-17 14:54:56 -0700

    bringing in components

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import Portfolio from './pages/Portfolio'
+import Contact from './pages/Contact'
+import {
+  Collapse, 
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+  Container,
+  Row,
+  Col,
+  Jumbotron,
+  Button
+} from 'reactstrap'
+
+const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen)
+
+  return (
+    <Router>
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand >reactstrap</NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink href="/portfolio/">Portfolio</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/contact/">Contact</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/marcymar">GitHub</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+export default App

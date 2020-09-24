02cecd7ee9038ac78a0a2d34b63aa6e2028aafe7
Marcella Luz Ash <marcella@ash-photos.com>
2020-09-24 11:21:53 -0700

    Updates

---
+++ precache-manifest.50c858cf63dfc720b6e554888c098a0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5cc033ea561f5393d2e4dcd0ee8dc5c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5adf353d05a0ca8ac959",
+    "url": "/portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5005d0f1b563a9c4763f",
+    "url": "/portfolio/static/css/main.a63c9040.chunk.css"
+  },
+  {
+    "revision": "5adf353d05a0ca8ac959",
+    "url": "/portfolio/static/js/2.c57ff903.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.c57ff903.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5005d0f1b563a9c4763f",
+    "url": "/portfolio/static/js/main.1d54aa56.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
